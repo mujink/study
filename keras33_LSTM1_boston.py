1585a99b5249c02fafb5ceeb158c7932a4138473
mujink <76673219+mujink@users.noreply.github.com>
2021-01-11 20:55:19 +0900

    Add files via upload
    1.11 p2

--- keras33_LSTM1_boston.py
+++ keras33_LSTM1_boston.py
@@ -3,54 +3,41 @@
 #  덴스와 성능 비교
 # 회귀
 
-#1. Data
+#1. DATA
+from tensorflow.keras.datasets import boston_housing
 import numpy as np
-from sklearn.datasets import load_boston
 
-dataset = load_boston()
-x = dataset.data
-y = dataset.target
-print(x.shape) #(506, 13)
-print(y.shape) #(506,)
-print('====================')
+(x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+# print(x_train.shape) #(404, 13)
 
-
-# 1.1 Data Preprocessing
-# MinMaxScaler 필수
-
-
+#1.1 Data Preprocessing
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, shuffle=True, train_size=0.8, random_state=33
-)
-x_train, x_val, y_train, y_val = train_test_split(
-    x_train, y_train,  test_size=0.2
-)
 
-# x의 값들을 0~1 사이 값으로 줄임 => 가중치를 낮추어 연산 속도가 빨라짐.
+x_train, x_validation, y_train, y_validation = train_test_split(
+    x_train, y_train, train_size = 0.8, shuffle = True, random_state=66)
+
+print(x_train.shape)    #(323, 13)
+print(x_test.shape)     #(102, 13)
+print(np.min(x_train), np.max(x_train)) # 0.0 ~ 711.0
+
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 scaler.fit(x_train)
-x_train = scaler.transform(x_train)
+x_train = scaler.transform(x_train)     
 x_test = scaler.transform(x_test)
-x_val = scaler.transform(x_val)
+x_validation = scaler.transform(x_validation)
 
-# print('====================')
-# x = x_train.reshape(x_train.shape[0],x_train.shape[1],1)
-# x = x_test.reshape(x_test.shape[0],x_test.shape[1],1)
-# x = x_val.reshape(x_val.shape[0],x_val.shape[1],1)
-# print("x_train.shape : " , x_train.shape) #(323, 13, 1)
-# print("x_test.shape : " , x_test.shape) #(102, 13, 1)
-# print("x_val.shape : " , x_val.shape) #(81, 13, 1)
-# print('====================')
-# 2. model
+x_train = x_train.reshape(x_train.shape[0],x_train.shape[1],1)
+x_test = x_test.reshape(x_test.shape[0],x_test.shape[1],1)
+x_validation = x_validation.reshape(x_validation.shape[0],x_validation.shape[1],1)
+
 
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Input, LSTM
 
 
 input1 = Input(shape=(13,))
-d1 = Dense(30, activation='relu')(input1)
+d1 = LSTM(30, activation='relu')(input1)
 dh = Dense(50, activation='relu')(d1)
 dh = Dense(100, activation='relu')(dh)
 dh = Dense(100, activation='relu')(dh)
@@ -95,4 +82,3 @@ r2_m1 = r2_score(y_test, y_predict)
 
 print("R2 :", r2_m1 )
 
-\
