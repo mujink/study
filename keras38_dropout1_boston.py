0e37f39ec4aeb9a95728074eec3ec5d5d834d650
mujink <76673219+mujink@users.noreply.github.com>
2021-01-12 09:11:34 +0900

    Update keras38_dropout1_boston.py
    re01 1.12

--- keras38_dropout1_boston.py
+++ keras38_dropout1_boston.py
@@ -1,131 +1,108 @@
-수정필요
 # EarlyStopping : 조건에 맞으면 epochs를 멈춤
 # model.fit에서 실행
 # #1. Data
+
 import numpy as np
-from sklearn.datasets import load_boston
+
+from sklearn.datasets import load_boston #보스턴 집값에 대한 데이터 셋을 교육용으로 제공하고 있다.
 
 dataset = load_boston()
-x = dataset.data
-y = dataset.target
-print(x.shape) #(506, 13)
-print(y.shape) #(506,)
-print('====================')
-print(x[:5])
-print(y[:10])
 
-print(np.max(x[0]))
+#1. DATA
+
+x = dataset.data
+y = dataset.target # target : x와 y 가 분리한다.
+
+# 다 : 1 mlp 모델을 구성하시오
+
+# print(x.shape)  # (506, 13) input = 13
+# print(y.shape)  # (506, )   output = 1
 
 # 1.1 Data Preprocessing
 # MinMaxScaler 필수
 
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, shuffle=True, train_size=0.8, random_state=33
-)
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size = 0.8, shuffle = True, random_state=66)
+x_train, x_validation, y_train, y_validation = train_test_split(x_train, y_train, train_size = 0.8, shuffle = True, random_state=66)
 
-from sklearn.model_selection import train_test_split
-x_train, x_val, y_train, y_val = train_test_split(
-    x_train, y_train,  test_size=0.2
-)
-
-# x의 값들을 0~1 사이 값으로 줄임 => 가중치를 낮추어 연산 속도가 빨라짐.
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 scaler.fit(x_train)
-
-x_train = scaler.transform(x_train)
+x_train = scaler.transform(x_train)    
 x_test = scaler.transform(x_test)
-x_val = scaler.transform(x_val)
+x_validation = scaler.transform(x_validation)
+
+print(np.max(x), np.min(x)) # 최댓값 711.0, 최솟값 0.0      ----> 최댓값 1.0 , 최솟값 0.0
+print(np.max(x[0]))         # max = 396.9
+
 
 
 # 2. model
 
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, Input, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
 
+model = Sequential()
+model.add(Dense(128, activation='relu',input_dim=13))
+model.add(Dropout(0.2))
+model.add(Dense(64))
+model.add(Dropout(0.2))
+model.add(Dense(64))
+model.add(Dropout(0.2))
+model.add(Dense(64))
+model.add(Dropout(0.2))
+model.add(Dense(64))
+model.add(Dropout(0.2))
+model.add(Dense(32))
+model.add(Dropout(0.2))
+model.add(Dense(32))
+model.add(Dropout(0.2))
+model.add(Dense(32))
+model.add(Dropout(0.2))
+model.add(Dense(32))
+model.add(Dropout(0.2))
+model.add(Dense(16))
+model.add(Dropout(0.2))
+model.add(Dense(1))
 
-input1 = Input(shape=(13,))
-d1 = Dense(1000, activation='relu')(input1)
-dh = Dropout(0.1)(d1)
-dh = Dense(500, activation='relu')(dh)
-dh = Dropout(0.1)(dh)
-dh = Dense(500, activation='relu')(dh)
-dh = Dropout(0.1)(dh)
-dh = Dense(500, activation='relu')(dh)
-dh = Dropout(0.1)(dh)
-dh = Dense(20, activation='relu')(dh)
-outputs = Dense(1)(dh)
-
-# 2.1 model def
-model = Model(inputs =  input1, outputs = outputs)
-model.summary()
-
-# 3. Compile, run
+#3. Compile, Train
 model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+
 from tensorflow.keras.callbacks import EarlyStopping
-early_stopping = EarlyStopping(monitor='loss', patience=100, mode='min')
+early_stopping = EarlyStopping(monitor='loss', patience=20, mode='min') 
+#                               loss를 모니터링한다. / 가장 낮은 loss가 나온 후 20번만 더 시행한 후, 그때까지도 최솟값 변화가 없다면 스탑 / min, max, auto
+# 장점 : 효율이 좋다. 얼추 효율이 좋은 epochs 위치를 알 수 있다. 정지할 수 있다. epochs 조절이 가능하다.
 
-model.fit(
-    x_train,y_train,
-    epochs=1000, batch_size= 10, validation_data=(x_val, y_val), verbose=2, callbacks=[early_stopping])
+model.fit(x_train, y_train, epochs=2000, batch_size=8, validation_data=(x_validation, y_validation), verbose=1, callbacks=[early_stopping])
 
-# 4 Evaluation validation
-loss, mae= model.evaluate(x_test, y_test, batch_size=1)
-print('loss : ', loss)
-print('mae : ', mae)
+#4. Evaluate, Predict
+loss, mae = model.evaluate(x_test, y_test, batch_size=8)
+print("loss : ", loss)
+print("mae : ", mae)
 
 y_predict = model.predict(x_test)
+# print("보스턴 집 값 : \n", y_predict)
 
+# RMSE
 from sklearn.metrics import mean_squared_error
-def RMSE(y_test, y_predict):
-    return np.sqrt(mean_squared_error(y_test, y_predict))
+def RMSE (y_test, y_train) :
+    return np.sqrt(mean_squared_error(y_test, y_train))
+print("RMSE : ", RMSE(y_test, y_predict))
 
-print("RMSE :", RMSE(y_test, y_predict) )
-
-# mse... 
-MSE1 = mean_squared_error(y_test, y_predict)
-
-print("RMSE :", MSE1 )
-
+# R2
 from sklearn.metrics import r2_score
-r2_m1 = r2_score(y_test, y_predict)
+R2 = r2_score(y_test, y_predict)
+print("R2 : ", R2)
 
-print("R2 :", r2_m1 )
+# early_stopping (20)  : 211번 실행
+# loss :  6.976583957672119
+# mae :  2.0358965396881104
+# RMSE :  2.6413223452327177
+# R2 :  0.91653100556001
 
-# 제대로 전처리 (validation_data)
-# 결과 값
-"""
-loss :  10.806500434875488
-RMSE : 3.2873240846554324
-RMSE : 10.806499637555676
-R2 : 0.849143856752024
-
-loss :  2.1226985454559326
-RMSE : 3.7007119725686497
-RMSE : 13.695269103912945
-R2 : 0.8088173278071026
-"""
-
-"""
-Dropout (너무 많이 줄였음 0.2)
-loss :  16.991928100585938
-mae :  2.659029245376587
-RMSE : 4.122126008917869
-RMSE : 16.99192283339716
-R2 : 0.7627968323706604
-
-loss :  16.991928100585938
-mae :  2.659029245376587
-RMSE : 4.122126008917869
-RMSE : 16.99192283339716
-R2 : 0.7627968323706604
-
-ropout (너무 많이 줄였음 0.1)
-loss :  13.841684341430664
-mae :  2.5528340339660645
-RMSE : 3.7204423956739734
-RMSE : 13.841691619528294
-R2 : 0.8067733046052109
-"""
+# Dropout (성능 조금 좋아짐)
+# loss :  5.84281587600708
+# mae :  1.7922717332839966
+# RMSE :  2.4171915790096543
+# R2 :  0.9300955994589993
