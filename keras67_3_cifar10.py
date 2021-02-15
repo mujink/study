17c3e376cc5c328ddcaca820cd7a5e4595816b8a
mujink <76673219+mujink@users.noreply.github.com>
2021-02-15 09:40:55 +0900

    Add files via upload
    2.15

--- keras67_3_cifar10.py
+++ keras67_3_cifar10.py
@@ -12,6 +12,9 @@ import matplotlib.pyplot as plt
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
+print(x_train.shape)
+print(x_test.shape)
+
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
@@ -44,18 +47,18 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Input, Conv2D, Flatten, MaxPool2D, BatchNormalization, AveragePooling2D
 
 model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=(10,10),input_shape=(32,32,3),padding='same',activation='relu'))
+model.add(Conv2D(filters=64, kernel_size=(10,10),input_shape=(32,32,3),padding='same',activation='relu'))
 model.add(MaxPool2D(pool_size=(2,2),strides = (2,2),padding='same'))
 model.add(BatchNormalization())
-model.add(Conv2D(filters=8, kernel_size=(5,5),padding='same',activation='relu'))
-model.add(AveragePooling2D((2,2),(2,2)))
+model.add(Conv2D(filters=32, kernel_size=(5,5),padding='same',activation='relu'))
+# model.add(AveragePooling2D((2,2),(2,2)))
 model.add(BatchNormalization())
 # model.add(Conv2D(filters=4, kernel_size=(3,3),padding='same',activation='relu'))
 # model.add(MaxPool2D(pool_size=(2,2),strides = (2,2),padding='same'))
 # model.add(BatchNormalization())
 model.add(Flatten())
+model.add(Dense(64,activation='relu'))
 model.add(Dense(32,activation='relu'))
-model.add(Dense(16,activation='relu'))
 model.add(Dense(20,activation='relu'))
 model.add(Dense(10,activation='softmax'))
 model.summary()
@@ -69,7 +72,11 @@ bath = 500
 
 history = model.fit(datagen.flow(x_train, y_train, batch_size=bath),
                     steps_per_epoch=len(x_train) / bath, epochs=epochs)
+# history = model.fit_generator(
 
+#     xy_train, steps_per_epoch=10, epochs=10,validation_data=xy_val,
+#     validation_steps=4
+# )
 """
 here's a more "manual" example
 for e in range(epochs):
@@ -108,15 +115,20 @@ print(history.history.keys()) #dict_keys(['loss', 'acc', 'val_loss', 'val_acc'])
 
 import matplotlib.pyplot as plt
 plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
+# plt.plot(history.history['val_loss'])
 plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
+# plt.plot(history.history['val_acc'])
 plt.title('cnn fashion_mnist')
 plt.ylabel('loss, acc')
 plt.xlabel('epoch')
-plt.legend(['train loss', 'val_loss', 'acc','val_acc'])
+plt.legend(['train loss', 'acc'])
 plt.show()
 
 """
-
+acc : 0.44001999497413635
+evl_loss :  329.9654541015625
+evl_acc :  0.1347000002861023
+R2 : -0.9208428758122565
+<tensorflow.python.keras.callbacks.History object at 0x000001EE7CA82B20>
+dict_keys(['loss', 'acc'])
 """
